37ca9bc1bb9c6e90198a9904daae5e492aa75a85
Roshan Gautam <Roshan Gautam>
2016-10-27 02:49:44 -0500

    Added some stuffs

--- HangMan/src/HangMan.java
+++ HangMan/src/HangMan.java
@@ -6,17 +6,36 @@
  * 2. Add more words.
  * 3. Maybe provide some hints for the word 
  */
+import java.io.*;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class HangMan {
 	public static void main(String[]args){
-		java.util.Scanner input = new java.util.Scanner(System.in);
+		File WORDS = new File("Hangman_Words.txt");
+		ArrayList<String> words = new ArrayList<String>();
+		String word;
+		Random random=new Random();
+		try{
+			Scanner inputfile = new Scanner(WORDS);
+		while (inputfile.hasNextLine()){
+			word = inputfile.nextLine();
+			words.add(word);
+		}
+		}
+		catch (Exception e)
+		{
+			System.out.println("The input file not found" + e.toString());
+		}
+		
+		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to hangman!");
 		System.out.println("Please enter (1) to Start the game or (0) to exit");
 		int command = input.nextInt();
 		String refresh="";
 		String Gist=refresh;
 		while (command == 1){
-			String [] words = {"doctor", "pilot", "engineer", "teacher"};
-			Gist=words[(int)(Math.random()*words.length)];
+			Gist = words.get(random.nextInt(words.size()));
 			System.out.println(Gist);
 			// I added this statement just for debugging purpose.
 			char [] letters = new char [Gist.length()];
@@ -43,11 +62,6 @@ public class HangMan {
 			}
 			System.out.println(tempGist);
 	}
-			
-			
-			
-			
-			
 		System.out.println("please enter (1) to continue to next word or (0) to exit");
 		command = input.nextInt();
 	}
