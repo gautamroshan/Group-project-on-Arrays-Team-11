da30162ca896abf1cf6fbd5aa4063ad4c8e0a790
Roshan Gautam <Roshan Gautam>
2016-10-27 19:22:42 -0500

    Please add a method or edit the code for giving number of chances and counting them.

--- HangMan/src/HangMan.java
+++ HangMan/src/HangMan.java
@@ -1,9 +1,5 @@
 /*Hi Guys,
- * I decided to take lead on the program number 7.35
- * We need to add the following features to our program.
- * 1. Counting the missed letters.
- * 2. Putting an end to the game with some message or option for playing again
- * 3. Maybe provide some hints for the words 
+ *Please add a method or edit the code for giving number of chances and counting them.
  */
 import java.io.*;
 import java.util.Random;
@@ -31,40 +27,48 @@ public class HangMan {
 		System.out.println("Welcome to hangman!");
 		System.out.println("Please enter (1) to Start the game or (0) to exit");
 		int command = input.nextInt();
+		if (command==0){
+			System.out.println("Thank you! Please visit again!");
+			System.exit(0);
+		}
 		String refresh="";
 		String Gist=refresh;
 		while (command == 1){
 			Gist = words.get(random.nextInt(words.size()));
 			System.out.println(Gist);
 			// I added this statement just for debugging purpose.
-			char [] letters = new char [Gist.length()];
-			for (int i=0; i< Gist.length(); i++){
-				letters[i]= Gist.charAt(i);
-			}
+			char [] letters = Gist.toCharArray();
 			String tempGist=refresh;
 			for (int j=0; j<Gist.length(); j++){
 				tempGist=tempGist + "*";
 			}
-			while (tempGist != Gist){
 				String OldGist = tempGist;
+				while(tempGist!=Gist){
 			System.out.println("Enter a letter in word: " + tempGist);
 			char deko=input.next().charAt(0);
 			tempGist=refresh;
 			for (int k=0; k<Gist.length(); k++ ){
 				if (deko == letters[k]){
-					tempGist= tempGist + deko;
+					tempGist+= deko;
 				}
 				else {
 					tempGist= tempGist + OldGist.charAt(k);
 					
 				}
 			}
-			System.out.println(tempGist);
+			OldGist=tempGist;
+			if (OldGist.matches(Gist)){
+				System.out.println(Gist);
+				System.out.println("You got it right!");
+				break;
+			}
 	}
-		System.out.println("please enter (1) to continue to next word or (0) to exit");
-		command = input.nextInt();
+				System.out.println("please enter (1) to continue to next word or (0) to exit");
+				command = input.nextInt();
+				if (command==0){
+					System.out.println("Thank you! Please visit again!");
+					System.exit(0);
+				}
+		}
 	}
-
-}
-	
 }
