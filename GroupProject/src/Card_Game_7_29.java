d05ac7c2471a1707149a39a69d74cc2fff03d044
LAB <LAB>
2016-10-27 22:25:54 -0500

    Added comments

--- GroupProject/src/Card_Game_7_29.java
+++ GroupProject/src/Card_Game_7_29.java
@@ -1,9 +1,11 @@
+//Import Scanner and Vector
 import java.util.Scanner;
 import java.util.Vector;
 
 public class Card_Game_7_29 {
 
 	public static void main(String[] args) {
+		//Hello
 	System.out.println("Hello");
 
 	}
